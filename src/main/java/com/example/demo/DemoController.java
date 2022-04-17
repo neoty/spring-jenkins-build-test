8d3548032addc167b22864c20d2442edd246b223
neoty <neoty@naver.com>
2022-04-17 14:43:34 +0900

    init

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+    @GetMapping("/")
+    public Map<String, Object> index() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+        map.put("key4", "value4");
+
+        return map;
+    }
+}
